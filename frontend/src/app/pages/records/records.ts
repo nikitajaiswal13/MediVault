bf905bca9ed634bdbeffb3dccaefa3fcf73fc322
Nikita Jaiswal <nikkijaiswal535@gmail.com>
2026-02-21 17:12:56 +0530

    Record Function Done

--- frontend/src/app/pages/records/records.ts
+++ frontend/src/app/pages/records/records.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Record } from '../../services/record';
 
 @Component({
   selector: 'app-records',
@@ -6,6 +8,56 @@ import { Component } from '@angular/core';
   templateUrl: './records.html',
   styleUrl: './records.css',
 })
-export class Records {
+export class Records implements OnInit {
+    records: any[] = [];
+  patientId!: string;
 
+  recordType = '';
+  hospital = '';
+  date = '';
+  selectedFile!: File;
+
+  constructor(
+    private route: ActivatedRoute,
+    private recordService: Record
+  ) {}
+
+  ngOnInit() {
+    this.patientId = this.route.snapshot.paramMap.get('patientId')!;
+    this.loadRecords();
+  }
+
+  loadRecords() {
+    this.recordService.getRecords(this.patientId)
+      .subscribe((res: any) => {
+        this.records = res.data;
+      });
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  uploadRecord() {
+    const formData = new FormData();
+    formData.append('recordType', this.recordType);
+    formData.append('hospital', this.hospital);
+    formData.append('date', this.date);
+    formData.append('file', this.selectedFile);
+
+    this.recordService.createRecord(this.patientId, formData)
+      .subscribe(() => {
+        this.recordType = '';
+        this.hospital = '';
+        this.date = '';
+        this.loadRecords();
+      });
+  }
+
+  deleteRecord(id: string) {
+    this.recordService.deleteRecord(id)
+      .subscribe(() => {
+        this.loadRecords();
+      });
+  }
 }
---
+++ frontend/src/app/services/record.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Record {
+private baseUrl = 'http://localhost:3000/api/v1/records';
+
+  constructor(private http: HttpClient) {}
+
+  private getHeaders() {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+  }
+
+  getRecords(patientId: string) {
+    return this.http.get(`${this.baseUrl}/patients/${patientId}`, {
+      headers: this.getHeaders()
+    });
+  }
+
+  createRecord(patientId: string, formData: FormData) {
+    return this.http.post(`${this.baseUrl}/patients/${patientId}`, formData, {
+      headers: this.getHeaders()
+    });
+  }
+
+  deleteRecord(id: string) {
+    return this.http.delete(`${this.baseUrl}/${id}`, {
+      headers: this.getHeaders()
+    });
+  }
+}
