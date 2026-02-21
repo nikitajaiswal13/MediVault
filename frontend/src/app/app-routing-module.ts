2ef4c2f072d578aaa7265eea167431d031cc488e
Nikita Jaiswal <nikkijaiswal535@gmail.com>
2026-02-21 17:22:17 +0530

    Functions DOne

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { authGuard } from './guards/auth-guard';
+
 import { Login } from './pages/login/login';
 import { Signup } from './pages/signup/signup';
 import { Patients } from './pages/patients/patients';
@@ -10,8 +12,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'signup', component: Signup },
-  { path: 'patients', component: Patients },
-  { path: 'records/:patientId', component: Records }
+  { path: 'patients', component: Patients , canActivate: [authGuard]},
+  { path: 'records/:patientId', component: Records , canActivate: [authGuard] }
 ];
 
 @NgModule({
--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class Navbar {
 
+  constructor(private router: Router) {}
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
