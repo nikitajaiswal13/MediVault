41ed0020a5d7e1a94bf095ce09eeb032121f774b
Nikita Jaiswal <nikkijaiswal535@gmail.com>
2026-04-08 00:59:17 +0530

    Backend Updated

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -10,6 +10,6 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage , limits: { fileSize: 5 * 1024 * 1024 } }); 
 
 module.exports = upload;
