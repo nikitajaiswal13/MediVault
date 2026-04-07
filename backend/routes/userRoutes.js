59c111b4f6ae850e3577cf6987c15e8197c0ab3f
Nikita Jaiswal <nikkijaiswal535@gmail.com>
2026-04-08 00:55:10 +0530

    Routes Updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 router.post('/signup', authControllers.signup);
 router.post('/login', authControllers.login);
 
-// 🔒 All routes below this line are protected
 router.use(authControllers.protect);
 
 router.route('/').get(userControllers.getAllUsers);
